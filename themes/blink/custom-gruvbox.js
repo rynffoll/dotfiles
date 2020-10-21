eabb46da446610417cbecd2b0924f777129d6266
Ruslan Kamashev <rynffoll@gmail.com>
2020-10-21 21:51:51 +0300

    Add blink theme

---
+++ themes/blink/custom-gruvbox.js
@@ -0,0 +1,26 @@
+t.prefs_.set('color-palette-overrides',
+             [
+                 "#272727",
+                 "#fa4933",
+                 "#8d971b",
+                 "#d79920",
+                 "#3d8588",
+                 "#d4859b",
+                 "#689e6a",
+                 "#a79984",
+
+                 "#928373",
+                 "#fa4934",
+                 "#979817",
+                 "#fabc2e",
+                 "#83a597",
+                 "#d3859a",
+                 "#699c6a",
+                 "#a79985"
+             ]);
+
+t.prefs_.set('foreground-color', "#a79985");
+
+t.prefs_.set('background-color', "#272727");
+
+t.prefs_.set('cursor-color', 'rgba(235,219,178,0.5)');
