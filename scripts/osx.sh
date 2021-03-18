28a56d41cab3bd2231aa29a26234424fe730d0a4
Ruslan Kamashev <rynffoll@gmail.com>
2021-03-18 22:41:01 +0300

    Add Rectangle config

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
+# defaults
+defaults write NSGlobalDomain CGFontRenderingFontSmoothingDisabled -bool false
+defaults write NSGlobalDomain ApplePressAndHoldEnabled             -bool false
+
+defaults write com.knollsoft.Rectangle bottomHalf      '{ keyCode = 125; modifierFlags = 1835008; }'
+defaults write com.knollsoft.Rectangle bottomLeft      '{}'
+defaults write com.knollsoft.Rectangle bottomRight     '{}'
+defaults write com.knollsoft.Rectangle center          '{ keyCode = 36;  modifierFlags = 1835008; }'
+defaults write com.knollsoft.Rectangle centerThird     '{}'
+defaults write com.knollsoft.Rectangle firstThird      '{}'
+defaults write com.knollsoft.Rectangle firstTwoThirds  '{}'
+defaults write com.knollsoft.Rectangle larger          '{}'
+defaults write com.knollsoft.Rectangle lastThird       '{}'
+defaults write com.knollsoft.Rectangle lastTwoThirds   '{}'
+defaults write com.knollsoft.Rectangle leftHalf        '{ keyCode = 123; modifierFlags = 1835008; }'
+defaults write com.knollsoft.Rectangle maximize        '{}'
+defaults write com.knollsoft.Rectangle maximizeHeight  '{}'
+defaults write com.knollsoft.Rectangle nextDisplay     '{ keyCode = 124; modifierFlags = 1966080; }'
+defaults write com.knollsoft.Rectangle previousDisplay '{ keyCode = 123; modifierFlags = 1966080; }'
+defaults write com.knollsoft.Rectangle restore         '{ keyCode = 51;  modifierFlags = 1835008; }'
+defaults write com.knollsoft.Rectangle rightHalf       '{ keyCode = 124; modifierFlags = 1835008; }'
+defaults write com.knollsoft.Rectangle smaller         '{}'
+defaults write com.knollsoft.Rectangle topHalf         '{ keyCode = 126; modifierFlags = 1835008; }'
+defaults write com.knollsoft.Rectangle topLeft         '{}'
+defaults write com.knollsoft.Rectangle topRight        '{}'
+
 # dictionaries
 wget -c -O ~/Library/Spelling/ru_RU.aff "https://raw.githubusercontent.com/LibreOffice/dictionaries/master/ru_RU/ru_RU.aff"
 wget -c -O ~/Library/Spelling/ru_RU.dic "https://raw.githubusercontent.com/LibreOffice/dictionaries/master/ru_RU/ru_RU.dic"
 wget -c -O ~/Library/Spelling/en_US.aff "https://raw.githubusercontent.com/LibreOffice/dictionaries/master/en/en_US.aff"
 wget -c -O ~/Library/Spelling/en_US.dic "https://raw.githubusercontent.com/LibreOffice/dictionaries/master/en/en_US.dic"
-
-# defaults
-defaults write NSGlobalDomain CGFontRenderingFontSmoothingDisabled -bool false
-echo "NSGlobalDomain/CGFontRenderingFontSmoothingDisabled -> $(defaults read NSGlobalDomain CGFontRenderingFontSmoothingDisabled)"
-
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-echo "NSGlobalDomain/ApplePressAndHoldEnabled -> $(defaults read NSGlobalDomain ApplePressAndHoldEnabled)"
