226ebc43fc98167f9cc8639a577908008e6dfefb
Ruslan Kamashev <rynffoll@gmail.com>
2024-01-27 15:55:36 +0400

    Update scripts/macos.sh

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -2,15 +2,44 @@
 
 # https://macos-defaults.com/
 
-# defaults
-defaults write NSGlobalDomain CGFontRenderingFontSmoothingDisabled -bool false
-defaults write NSGlobalDomain ApplePressAndHoldEnabled             -bool false
+# Keyboard
 
-# https://stackoverflow.com/questions/77248249/disable-macos-sonoma-text-insertion-point-cursor-caps-lock-indicator/77296786#77296786
-sudo defaults write /Library/Preferences/FeatureFlags/Domain/UIKit.plist redesigned_text_cursor -dict-add Enabled -bool false
+# https://mac-key-repeat.zaymon.dev/
+defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write -g InitialKeyRepeat         -int 15 # Delay until repeat
+defaults write -g KeyRepeat                -int 2  # Key repeat rate
+
+defaults write kCFPreferencesAnyApplication TSMLanguageIndicatorEnabled -int 0 # Sonoma: Disable language popup
+
+# Dock
+defaults write com.apple.dock autohide -bool true
+
+killall Dock
+
+# Mission Control
+defaults write com.apple.dock mru-spaces -bool false # Automatic rearrange Spaces based on most recent use
+
+killall Dock
+
+# Finder
+defaults write -g AppleShowAllExtensions    -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+
+killall Finder
+
+# Safari
+defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
+
+killall Safari && open -a Safari
 
 # iTerm2
-defaults write com.googlecode.iterm2 JiggleTTYSizeOnClearBuffer -bool true
+defaults write com.googlecode.iterm2 TabViewType                 -int 2     # Appearance / General / Tab bar location - Left
+defaults write com.googlecode.iterm2 TabStyleWithAutomaticOption -int 5     # Appearance / General / Theme - Minimal
+defaults write com.googlecode.iterm2 SwitchPaneModifier          -int 5     # Keys / Navigation Shortcuts / Shorcut to choose a split pane - ⌥ Number
+defaults write com.googlecode.iterm2 JiggleTTYSizeOnClearBuffer  -bool true # Respects multi-line prompt on clear buffer
+
+/usr/libexec/PlistBuddy -c "Delete :'New Bookmarks':0:'Unlimited Scrollback'" ~/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c "Add :'New Bookmarks':0:'Unlimited Scrollback' bool true" ~/Library/Preferences/com.googlecode.iterm2.plist
 
 # Rectangle
 defaults write com.knollsoft.Rectangle bottomHalf      '{ keyCode = 125; modifierFlags = 1835008; }'
@@ -35,15 +64,16 @@ defaults write com.knollsoft.Rectangle topHalf         '{ keyCode = 126; modifie
 defaults write com.knollsoft.Rectangle topLeft         '{}'
 defaults write com.knollsoft.Rectangle topRight        '{}'
 
-pkill Rectangle && open -a Rectangle
+killall Rectangle && open -a Rectangle
 
 # Raycast
 defaults write com.raycast.macos raycastPreferredWindowMode -string 'compact'
 defaults write com.raycast.macos navigationCommandStyleIdentifierKey -string 'vim'
 
-pkill Raycast && open -a Raycast
+killall Raycast && open -a Raycast
 
-# dictionaries
+
+# Dictionaries
 curl -sS -C - -o ~/Library/Spelling/ru_RU.aff "https://raw.githubusercontent.com/LibreOffice/dictionaries/master/ru_RU/ru_RU.aff"
 curl -sS -C - -o ~/Library/Spelling/ru_RU.dic "https://raw.githubusercontent.com/LibreOffice/dictionaries/master/ru_RU/ru_RU.dic"
 curl -sS -C - -o ~/Library/Spelling/en_US.aff "https://raw.githubusercontent.com/LibreOffice/dictionaries/master/en/en_US.aff"
