3559bd37a1465e219f4ccd26cf7d9e13d467861f
Ruslan Kamashev <rynffoll@gmail.com>
2025-10-31 22:47:33 +0400

    Revert "Add lm-studio"
    This reverts commit d62d15281f70311dcb597a8cf342bbc7e9457bd3.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,7 +7,6 @@ fish_add_path $HOME/bin
 fish_add_path $HOME/go/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
-fish_add_path $HOME/.lmstudio/bin
 
 if status is-interactive
     type -q mise; and mise activate fish | source
